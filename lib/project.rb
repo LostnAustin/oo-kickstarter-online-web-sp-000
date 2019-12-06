26eed6f2072c9d6acec288b0d68ee9f4a2c9fb39
Chad Lengefeld <chadlengefeld@gmail.com>
2019-12-06 16:03:45 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,18 @@
+class Project
+  attr_reader :backers, :title
+
+  def initialize(title)
+    @title = title
+    @backers = []
+  end
+
+  def add_backer(backer)
+    investor = Backer.new(self)
+    @backers << backer
+  end
+
+  def back_project
+    @backers << investor
+  end
+
+end
